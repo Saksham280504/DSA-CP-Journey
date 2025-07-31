0f3acc6142003adbd591d0a56ba112c2d06650f2
Saksham280504 <sakshammodi47@gmail.com>
2025-07-31 19:52:37 +0530

    Completed the P03 of Day1

--- Question-Practice/TLE-Eliminators Sheet/Day1/P03/P03.cpp
+++ Question-Practice/TLE-Eliminators Sheet/Day1/P03/P03.cpp
@@ -22,10 +22,41 @@ int main() {
     while(t--) {
         ll a,b;
         cin >> a >> b;
-        ll xk, yk;
-        cin >> xk >> yk;
-        ll xq, yq;
-        cin >> xq >> yq;
+        pair<ll,ll> king;
+        cin >> king.first >> king.second;
+        pair<ll,ll> queen;
+        cin >> queen.first >> queen.second;
+        set<pair<ll,ll>> stk;
+        set<pair<ll,ll>> stfinal;
+        vector<pair<ll,ll>> move;
+
+        move.push_back({-1*a,b});
+        move.push_back({a,-1*b});
+        move.push_back({-1*a,-1*b});
+        move.push_back({a,b});
+        move.push_back({-1*b,a});
+        move.push_back({b,-1*a});
+        move.push_back({-1*b,-1*a});
+        move.push_back({b,a});
+
+        for(int i=0; i<8; i++) {
+            ll xk = king.first;
+            ll yk = king.second;
+            xk += move[i].first;
+            yk += move[i].second;
+            stk.insert({xk,yk});
+        }
+        for(int i=0; i<8; i++) {
+            ll xq = queen.first;
+            ll yq = queen.second;
+            xq += move[i].first;
+            yq += move[i].second;
+            if(stk.find({xq,yq})!=stk.end()) {
+                stfinal.insert({xq,yq});
+            }
+        }
+
+        cout << stfinal.size() << endl;
     }
 
     return 0;
