258021a6b94ad4dd06fda68fd14d5d3d4af7d871
Saksham280504 <sakshammodi47@gmail.com>
2025-08-22 17:18:23 +0530

    Completed P14 900 Rated

--- Question-Practice/TLE-Eliminators Sheet/900 Rated Problems/P14/P14.cpp
+++ Question-Practice/TLE-Eliminators Sheet/900 Rated Problems/P14/P14.cpp
@@ -7,25 +7,15 @@ using ll = long long;
 void solve() {
     ll n;
     cin >> n;
-    bool isMinPossible = true;
-    bool isMaxPossible = true;
-    // For Minimum Number of buses
-
-    ll remaining1 = n%6;
-    ll MinBuses = n/6;
-    if(remaining1%4!=0) isMinPossible = false;
-    else if(remaining1!=0) MinBuses += 1;
-
-    // For Maximum Number of buses
-
-    ll remaining2 = n%4;
-    ll MaxBuses = n/4;
-    if(remaining2!=0) isMaxPossible = false;
-
-    if(isMinPossible==false && isMaxPossible== false) cout << -1 << endl;
-    else if(isMinPossible==false) cout << MaxBuses << " " << MaxBuses << endl;
-    else if(isMaxPossible==false) cout << MinBuses << " " << endl;
-    else cout << MinBuses << " " << MaxBuses << endl;
+    if(n%2!=0 || n<4) {
+        cout << -1 << endl;
+        return;
+    }
+    if(n%6==0) {
+        cout << n/6 << " " << n/4 << endl;
+        return;
+    }
+    cout << n/6 + 1 << " " << n/4 << endl;
 }
 int32_t main() {
     ios::sync_with_stdio(0);
