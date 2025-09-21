dd72eaf61ea84b55c1ab97a1797ba28532e534b8
Saksham280504 <sakshammodi47@gmail.com>
2025-09-21 23:31:52 +0530

    DP02 Completed

--- StriverA2Z/L14 Dynamic Programming/L14DP03/L14DP03.cpp
+++ StriverA2Z/L14 Dynamic Programming/L14DP03/L14DP03.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
 // #define endl '/n' 
+int FrogJump(int ind, vector<int>& jump, vector<int>& dp) {
+    if(ind==0) return 0;
+    if(dp[ind]!=-1) return dp[ind];
+    int right = INT_MAX;
+    int left = FrogJump(ind-1,jump,dp) + abs(jump[ind]-jump[ind-1]);
+    if(ind>1) right = FrogJump(ind-2,jump,dp) + abs(jump[ind]-jump[ind-2]);
+    return dp[ind] = min(left,right);
 
+    // TC -> O(N)
+    // SC -> O(N) (stack-space) + O(N) (dp-array)
+}
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -16,9 +26,31 @@ int main() {
     // your code here
 
     // Our approach for this problem will be as follows:
-    // 1. Create an index 
+    // 1. Express the problem in terms of index 
     // 2. Do all stuffs on that index
     // 3. Take the minimum value of all stuffs
-    
+
+    int n;
+    cin >> n;
+    vector<int> jump(n);
+    for(int i=0; i<n; i++) {
+        cin >> jump[i];
+    }
+
+    // Memoization Approach:
+    // vector<int> dp(n+1,-1);
+    // int cnt = FrogJump(n-1,jump,dp);
+    // cout << cnt << endl;
+
+    vector<int> dp(n+1,-1);
+    dp[0] = 0;
+    for(int i=1; i<n; i++) {
+        int fs = dp[i-1] + abs(jump[i]-jump[i-1]);
+        int ss = INT_MAX;
+        if(i>1) ss = dp[i-2] + abs(jump[i]-jump[i-2]);
+        dp[i] = min(ss,fs);
+    }
+
+    cout << dp[n-1] << endl;
     return 0;
 }
