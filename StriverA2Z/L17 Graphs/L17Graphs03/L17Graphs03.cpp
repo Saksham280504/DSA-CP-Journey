ea4db42ad256d9862fba3cb577d504a77de1342e
Saksham280504 <sakshammodi47@gmail.com>
2025-11-30 15:19:34 +0530

    Completed till Graphs03

--- StriverA2Z/L17 Graphs/L17Graphs03/L17Graphs03.cpp
+++ StriverA2Z/L17 Graphs/L17Graphs03/L17Graphs03.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
-// #define endl '
-'
+// #define endl '/n'
 
 int main() {
     ios::sync_with_stdio(0);
@@ -16,5 +15,26 @@ int main() {
 
     // your code here
 
+    int n,m;
+    cin >> n >> m;
+
+    vector<int> adj[n+1];
+
+    for(int i=0; i<m; i++) {
+        int u,v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    // Traversal algorithm for graph with multiple components
+
+    vector<int> vis(n+1,0);
+
+    for(int i=1; i<=n; i++) {
+        if(!vis[i]) {
+            // traversal(i);
+        }
+    }
     return 0;
 }
