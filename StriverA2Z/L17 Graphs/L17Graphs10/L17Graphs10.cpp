22f4ba95729bfef39e1163efd7086485550d1217
Saksham280504 <sakshammodi47@gmail.com>
2025-12-01 23:57:56 +0530

    Completed till Graphs10

--- StriverA2Z/L17 Graphs/L17Graphs10/L17Graphs10.cpp
+++ StriverA2Z/L17 Graphs/L17Graphs10/L17Graphs10.cpp
@@ -1,9 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
-// #define endl '
-'
+// #define endl '/n'
+bool detect(int src, vector<int> adjLS[],vector<int>& vis) {
+    vis[src] = 1;
+    queue<pair<int,int>> q;
+    q.push({src,-1});
 
+    while(!q.empty()) {
+        int node = q.front().first;
+        int parent = q.front().second;
+        q.pop();
+        for(auto adjacentNode: adjLS[node]) {
+            if(!vis[adjacentNode]) {
+                vis[adjacentNode] = 1;
+                q.push({adjacentNode,node});
+            }
+            else if(parent != adjacentNode) return true; // If paths cross, then that means a cycle has been formed
+        }
+    }
+
+    return false;
+}
+bool isCycle(int n, vector<int> adjLS[]) {
+    vector<int> vis(n,0);
+    for(int i=0; i<n; i++) {
+        if(!vis[i]) {
+            if(detect(i,adjLS,vis)) return true;
+        }
+    }
+
+    return false;
+
+    // TC -> O(N) (for loop) + O(2E) (we check the queue for the entire degree of the graph)
+}
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -15,6 +45,16 @@ int main() {
 #endif
 
     // your code here
+
+    int n,m;
+    cin >> n >> m;
+    vector<int> adjLS[n];
+    for(int i=0; i<m; i++) {
+        int u,v;
+        cin >> u >> v;
+        adjLS[u].push_back(v);
+        adjLS[v].push_back(u);
+    }
 
     return 0;
 }
