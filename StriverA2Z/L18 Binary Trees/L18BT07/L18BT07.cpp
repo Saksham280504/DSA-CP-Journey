8840f2683ea65d8a8f79e5f4fde31d9a11983f5c
Saksham280504 <sakshammodi47@gmail.com>
2025-12-10 18:41:27 +0530

    Completed till L18BT07

---
+++ StriverA2Z/L18 Binary Trees/L18BT07/L18BT07.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+class Node {
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class Solution {
+    public:
+    vector<vector<int>> levelOrder(Node* root) {
+        queue<Node*> q;
+        vector<vector<int>> ans;
+        q.push(root);
+
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> temp;
+            for(int i=0; i<size; i++) {
+                Node* node = q.front();
+                q.pop();
+                if(node->left!=nullptr) q.push(node->left);
+                if(node->right!=nullptr) q.push(node->right);
+                temp.push_back(node->data);
+            }
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+    Solution sol;
+    vector<vector<int>> ans = sol.levelOrder(root);
+
+    for(auto x: ans) {
+        for(auto y: x) {
+            cout << y << " ";
+        }
+    }
+
+
+    return 0;
+}
