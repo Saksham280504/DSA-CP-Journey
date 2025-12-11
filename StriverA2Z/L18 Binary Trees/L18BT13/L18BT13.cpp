f921fbdf66a2269fa2324fb406e8c3b3206f9b5f
Saksham280504 <sakshammodi47@gmail.com>
2025-12-12 01:13:52 +0530

    Completed till L18BT14

--- StriverA2Z/L18 Binary Trees/L18BT13/L18BT13.cpp
+++ StriverA2Z/L18 Binary Trees/L18BT13/L18BT13.cpp
@@ -17,13 +17,36 @@ class Node {
 
 class Solution {
     public:
-    int MaxDepth(Node* node) {
+    int MaxDepth1(Node* node) {
         if(node==nullptr) return 0;
-        return 1 + max(MaxDepth(node->left),MaxDepth(node->right)); // If there exists a valid node, then that has height of atleast 1
+        return 1 + max(MaxDepth1(node->left),MaxDepth1(node->right)); // If there exists a valid node, then that has height of atleast 1
 
         // TC -> O(N)
-        // SC -> O(N) (worst case->skew DS)
+        // SC -> O(Height) -> O(N) (worst case->skew DS)
     }
+
+    int MaxDepth2(Node* node) {
+        if(node==nullptr) return 1;
+        queue<Node*> q;
+        q.push(node);
+        int height = 0;
+        while(!q.empty()) {
+            height++; // Whenever a while loop iteration starts running, we reach a new level
+            int size = q.size(); // At every new iteration of the while loop, we will have the elements of the new level only.
+            for(int i=0; i<size; i++) {
+                Node* temp = q.front();
+                q.pop();
+                if(temp->left!=nullptr) q.push(temp->left);
+                if(temp->right!=nullptr) q.push(temp->right);
+            }
+        }
+
+        return height;
+
+        // TC ->O(N)
+        // SC ->O(N)
+    }
+
 };
 int main() {
     ios::sync_with_stdio(0);
@@ -45,8 +68,11 @@ int main() {
     root->right->right = new Node(6);
 
     Solution sol;
-    int depth = sol.MaxDepth(root);
-    cout << depth << endl;
+    int depth1 = sol.MaxDepth1(root);
+    cout << depth1 << endl;
+
+    int depth2 = sol.MaxDepth2(root);
+    cout << depth2 << endl;
 
     return 0;
 }
---
+++ StriverA2Z/L18 Binary Trees/L18BT14/L18BT14.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+class Node {
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class Solution {
+    private: 
+    // int height (Node* node) {
+    //     if(node==nullptr) return 0;
+    //     int lh = height(node->left);
+    //     int rh = height(node->right);
+    //     return 1 + max(lh,rh);
+    // }
+    int height(Node* node) { // Here we have made some tweeks and now we only get the height when we see that the tree is a balanced tree
+        if(node==nullptr) return 0; // nullpointer node is also a balanced tree
+        int lh = height(node->left);
+        int rh = height(node->right);
+        if(lh==-1 || rh==-1) return -1; // If at any node we find out that the tree is not Balanced, we return -1
+        if(abs(lh-rh)>1) return -1; // If at any node we find out that the tree is not Balanced, we return -1
+        return 1 + max(lh,rh);
+    }
+    public:
+    // Brute-force solution
+    // bool check1(Node* node) {
+    //     if(node==nullptr) return true;
+
+    //     int lh = height(node->left);
+    //     int rh = height(node->right);
+
+    //     if(abs(lh-rh)>1) return false;
+    //     bool lc = check1(node->left);
+    //     bool rc = check1(node->right);
+    //     if(!lc || !rc) return false;
+    //     return true;
+
+    //     // TC -> O(N*N) (not favourable)
+    // }
+
+    // Optimized Approach (TC-> O(N))
+
+    bool check2(Node* root) {
+        if(height(root)<0) return false;
+        return true;
+
+        // TC -> O(N) (favourable)
+    }
+};
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->right->left = new Node(4);
+    root->right->right = new Node(5);
+
+    return 0;
+}
