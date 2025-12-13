4db55774628ceaa82e8dbfaf552891b1eb89c12c
Saksham280504 <sakshammodi47@gmail.com>
2025-12-14 00:45:36 +0530

    Lecture Completed

---
+++ StriverA2Z/L18 Binary Trees/L18BT32/L18BT32.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+
+    // Requirements to construct a unique binary tree:
+    // 1) Inorder traversal (Having an information of what is present on the right and left of the root helps the most in constructing the unique binary tree)
+    // 2) Pre/Postorder traversal
+
+    return 0;
+}
