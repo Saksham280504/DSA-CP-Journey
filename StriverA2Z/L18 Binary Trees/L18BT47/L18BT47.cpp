b52785d9d6200359c736ba4340b839da13581c26
Saksham280504 <sakshammodi47@gmail.com>
2025-12-15 00:47:27 +0530

    Lecture Completed

--- StriverA2Z/L18 Binary Trees/L18BT47/L18BT47.cpp
+++ StriverA2Z/L18 Binary Trees/L18BT47/L18BT47.cpp
@@ -15,24 +15,23 @@ class Node {
 };
 
 class Solution {
-    private:
-    Node* lca(Node* node, Node* p, Node* q) {
-        if(!node || node==p || node==q) return node;
-        Node* ln = lca(node->left,p,q);
-        Node* rn = lca(node->right,p,q);
-        if(!ln) return rn;
-        else if(!rn) return ln;
-        return node; // If at a node, both ln and rn exists, that would only happen at the LCA
-    }
     public:
     Node* lowestCommonAncestor(Node* root, Node* p, Node* q) {
-        if(!root || !p || !q) return nullptr;
-        return lca(root,p,q);
-    }
+        if(!root) return nullptr;
+        int curr = root->data;
+        if(curr<p->data && curr<q->data) { // Both p and q lies on the right of node
+            return lowestCommonAncestor(root->right,p,q);
+        }
+        if(curr>p->data && curr>q->data) { // Both p and q lies on the left of node
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        return root;
 
-    // TC -> O(N)
-    // SC -> O(height of tree)
+        // TC -> O(Height of tree)
+        // SC -> O(Height of tree) (Auxillary Stack space)
+    }
 };
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
