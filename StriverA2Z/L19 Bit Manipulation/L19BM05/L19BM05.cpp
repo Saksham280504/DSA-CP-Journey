904eb8793ec2d791e981ae737153ecda49034206
Saksham280504 <sakshammodi47@gmail.com>
2025-12-19 12:48:17 +0530

    Lecture Completed

--- StriverA2Z/L19 Bit Manipulation/L19BM05/L19BM05.cpp
+++ StriverA2Z/L19 Bit Manipulation/L19BM05/L19BM05.cpp
@@ -4,26 +4,14 @@ using namespace std;
 // #define endl '/n'
 
 class Solution {
-    public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        int n = nums.size();
-        int subsets = 1<<n;
-
-        for(int num = 0; num<subsets; num++) {
-            vector<int> subset;
-            for(int i=0; i<n; i++) {
-                if(num&(1<<i)) {
-                    subset.push_back(nums[i]);
-                }
-            }
-            ans.push_back(subset);
+public:
+    int singleNumber(vector<int>& nums) {
+        int x = 0;
+        for(auto it: nums) {
+            x = x^it;
         }
 
-        return ans;
-
-        // TC -> O(N*(2^N))
-        // SC -> O((2^N)*N)
+        return x;
     }
 };
 
