0d22572be344060a5fe0ffaeb54bf254113977fe
Saksham280504 <sakshammodi47@gmail.com>
2025-12-22 13:05:42 +0530

    Lecture Completed

---
+++ StriverA2Z/L13 Stacks & Queues/L13SQ09/L13SQ09.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+
+    // What is an operator? (^,*,/,+,-) -> (arithematic operators used for arithematic expressions)
+    // What is an operand? (A-Z, a-z, 0-9)
+    // What is priority order? (^(3), *,/(2), +,-(1), anything other than these(-1))
+    // Infix to Postfix
+    // Infix to Prefix
+    // Postfix to Infix
+    // Prefix to Infix
+    // Postfix to Prefix
+    // Prefix to Postfix
+
+    // Whenver there is prefix, there is reverse
+    // Prefix (operator comes first)
+    // Infix (operator comes between)
+    // Postfix (operator comes last)
+
+    return 0;
+}
