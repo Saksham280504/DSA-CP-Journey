b5462b03c663d4594cd21c302dd5e65e831d0f72
Saksham280504 <sakshammodi47@gmail.com>
2026-02-15 11:51:06 +0530

    Problem Solved

---
+++ Question-Practice/TLE-Eliminators Sheet/1200 Rated Problems/P24/P24.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int cnt = 0;
+    for(int i=0; i<n; i++) {
+        if(s[i]=='0') cnt++;
+    }
+    if(cnt%2==0 || cnt==1) {
+        cout << "BOB" << endl;
+    }
+    else cout << "ALICE" << endl;
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+    int t;
+    cin >> t;
+    while(t--) solve();
+
+    return 0;
+}
