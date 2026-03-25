fea6e913bf568a65ed896fb7a4fe36525fdb6423
Saksham280504 <sakshammodi47@gmail.com>
2026-03-25 13:48:52 +0530

    Problem Solved with complete solution

--- Question-Practice/Leetcode/Medium Sheet/P33/P33.cpp
+++ Question-Practice/Leetcode/Medium Sheet/P33/P33.cpp
@@ -1,8 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
-// #define endl '
-'
+// #define endl '/n'
+
+class Solution {
+    private:
+    bool isShipPossibleInDdays(vector<int>& weights, int days, int capacity) {
+        int requiredDays = 1;
+        int currLoad = 0;
+
+        for(int w: weights) {
+            if(currLoad+w > capacity) {
+                requiredDays++;
+                currLoad = 0;
+            }
+            currLoad += w;
+        }
+
+        return requiredDays<=days;
+    }
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(),0);
+
+        while(low<high) {
+            int mid = low + (high-low)/2;
+            if(isShipPossibleInDdays(weights,days,mid)) {
+                high = mid;
+            }
+            else low = mid+1;
+        }
+
+        return low;
+    }
+};
 
 int main() {
     ios::sync_with_stdio(0);
