0110c28bc73fda0f29cf0eae825187dc0429d195
Saksham280504 <sakshammodi47@gmail.com>
2026-03-27 16:20:24 +0530

    Problem Solved

--- Question-Practice/Leetcode/Medium Sheet/P50/P50.cpp
+++ Question-Practice/Leetcode/Medium Sheet/P50/P50.cpp
@@ -3,6 +3,32 @@ using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
 // #define endl '/n'
 
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+        if(n>m) return false;
+        vector<int> freq1(26,0), freq2(26,0);
+        for(char c: s1) {
+            freq1[c-'a']++;
+        }
+        for(int i=0; i<n; i++) { // s2[0...n-1]
+            freq2[s2[i]-'a']++;
+        }
+
+        if(freq1==freq2) return true;
+        for(int i=n; i<m; i++) {
+            freq2[s2[i]-'a']++;
+            freq2[s2[i-n]-'a']--;
+            if(freq1==freq2) return true;
+        }
+
+        return false;
+
+        // TC -> O(N+M)
+        // SC -> O(26) -> O(1)
+    }
+};
 
 int main() {
     ios::sync_with_stdio(0);
