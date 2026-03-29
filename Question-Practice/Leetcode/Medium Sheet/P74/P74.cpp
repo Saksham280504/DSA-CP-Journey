11907a93915d9cd61a72ae892ea4967e3a4d53ae
Saksham280504 <sakshammodi47@gmail.com>
2026-03-29 13:57:48 +0530

    Problem Solved

---
+++ Question-Practice/Leetcode/Medium Sheet/P74/P74.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+class TreeNode {
+    public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int data) {
+        val = data;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+class Solution {
+    public:
+    void recoverTree(TreeNode* root) {
+
+        // TC -> O(N)
+        // SC -> O(1) (without ASS)
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+
+    return 0;
+}
