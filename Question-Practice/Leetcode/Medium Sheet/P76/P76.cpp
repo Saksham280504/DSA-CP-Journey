212b042921181a1e59a68caf7cce54fd4c82f106
Saksham280504 <sakshammodi47@gmail.com>
2026-03-29 18:45:52 +0530

    Problem Solved

---
+++ Question-Practice/Leetcode/Medium Sheet/P76/P76.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+class TreeNode {
+    public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int data) {
+        val = data;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root==nullptr) return ans;
+        queue<TreeNode*> q;
+        bool leftToRight = true;
+        q.push(root);
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> temp(size);
+            for(int i= 0; i<size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left!=nullptr) q.push(node->left);
+                if(node->right!=nullptr) q.push(node->right);
+                int ind = leftToRight ? i : size-1-i;
+                temp[ind] = node->val;
+            }
+            leftToRight = !leftToRight;
+            ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+
+    return 0;
+}
