95dd0106799bf2fca069cbec9d72ed5e562d3ddb
Saksham280504 <sakshammodi47@gmail.com>
2026-03-30 11:21:13 +0530

    Problem Solved

---
+++ Question-Practice/Leetcode/Medium Sheet/P69/P69.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+class ListNode {
+    public:
+    int val;
+    ListNode* next;
+    ListNode() {
+        val = 0;
+        next = nullptr;
+    }
+};
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+
+    return 0;
+}
