03d7848f6bbcb3eba9b595d9be31314b80d2b9bc
Saksham280504 <sakshammodi47@gmail.com>
2026-04-05 21:49:21 +0530

    Problem Solved

---
+++ Question-Practice/Leetcode/Medium Sheet/P104/P104.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+class Solution {
+    public:
+    bool checkValidString(string s) {
+        int mini = 0;
+        int maxi = 0;
+
+        for(auto it: s) {
+            if(it=='(') {
+                mini++;
+                maxi++;
+            }
+            else if(it==')') {
+                mini--;
+                maxi--;
+            }
+            else { // it=='*'
+                mini--; 
+                maxi++; // Our main aim is to create the largest possible positive range of values for a string.
+            }
+            if(mini<0) mini = 0; // mini was taking the maximum chances of reducing itself, so if mini somehow reaches < 0, that means that when we took the path with maximum -1s(')'), that's when we came <0, we can prevent that condition by reducing one -1s to make mini again equal to 0
+            if(maxi<0) return false; // maxi was taking minimum chances of reducing itself, so if in that path too maxi becomes <0, then that string is for sure not valid as there is no astric('*') to prevent it.
+        }
+        if(mini==0) return true;
+        return false;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+
+    return 0;
+}
