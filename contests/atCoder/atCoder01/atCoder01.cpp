cb774080c362b2a9ddad4336d50264ee6ad2c46d
Saksham280504 <sakshammodi47@gmail.com>
2026-05-02 17:37:25 +0530

    Atcoder01 Problem01 Solved

---
+++ contests/atCoder/atCoder01/atCoder01.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+    int x;
+    cin >> x;
+    if(x<3 || x>18) cout << "No" << endl;
+    else cout << "Yes" << endl;
+
+    return 0;
+}
