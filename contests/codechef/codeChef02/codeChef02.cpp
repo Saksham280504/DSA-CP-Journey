4d810b96a8f64d1e6af54e8bc8f7b9c195d43a5b
Saksham280504 <sakshammodi47@gmail.com>
2026-05-13 21:52:00 +0530

    Problem Solved

--- contests/codechef/codeChef02/codeChef02.cpp
+++ contests/codechef/codeChef02/codeChef02.cpp
@@ -46,22 +46,71 @@ using namespace std;
 // }
 
 // Q4
-void solve4() {
-    int n, m;
-    cin >> n >> m;
-    if((n-2)%3!=0) {
-        cout << -1 << endl;
-        return;
+// void solve4() {
+//     int n, m;
+//     cin >> n >> m;
+//     if((n-2)%3!=0) {
+//         cout << -1 << endl;
+//         return;
+//     }
+//     for(int i=1; i<=n; i++) {
+//         if(i%3==1 && i&1) cout << 1 << " ";
+//         else if(i%3==1) cout << m-1 << " ";
+//         else if(i%3==2 && i%2==0) cout << 1 << " ";
+//         else if(i%3==2) cout << m-1 << " ";
+//         else cout << 0 << " ";
+//     }
+//     cout << endl;
+// }
+
+// Q5
+#include <bits/stdc++.h>
+using namespace std;
+
+int min_max(vector<int>& temp, int m) {
+    if(m==1) return temp[0];
+    vector<int> temp1(m-1,0);
+    if(m&1) {
+        for(int i=0; i<m-1; i++) temp1[i] = min(temp[i],temp[i+1]);
+        int ans = min_max(temp1,m-1);
+        temp1.clear();
+        return ans;
     }
-    for(int i=1; i<=n; i++) {
-        if(i%3==1 && i&1) cout << 1 << " ";
-        else if(i%3==1) cout << m-1 << " ";
-        else if(i%3==2 && i%2==0) cout << 1 << " ";
-        else if(i%3==2) cout << m-1 << " ";
-        else cout << 0 << " ";
-    }
-    cout << endl;
+    for(int i=0; i<m-1; i++) temp1[i] = max(temp[i],temp[i+1]);
+    int ans = min_max(temp1,m-1);
+    temp1.clear();
+    return ans;
 }
+void solve5() {
+    int n,q;
+    cin >> n >> q;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++) cin >> arr[i];
+    while(q--) {
+        int type, q1, q2;
+        cin >> type >> q1 >> q2;
+        if(type==1) arr[q1-1] = q2;
+        else {
+            q1--;
+            q2--;
+            vector<int> temp;
+            for(int i=q1; i<=q2; i++) temp.push_back(arr[i]);
+            cout << min_max(temp, q2-q1+1) << endl;
+            temp.clear();
+        }
+    }
+
+    // This solution is giving TLE
+}
+
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--) solve5();
+}
+
+
 
 int main() {
     ios::sync_with_stdio(0);
@@ -76,6 +125,6 @@ int main() {
     // your code here
     int t;
     cin >> t;
-    while(t--) solve4();
+    while(t--) solve5();
     return 0;
 }
