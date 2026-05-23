2f5caeccd592ea0b309a1ed2a303936ebf431a17
Saksham280504 <sakshammodi47@gmail.com>
2026-05-23 17:37:15 +0530

    Contest Question01 done

---
+++ contests/atCoder/atCoder02/atCoder02.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+
+    // Q1
+    int x;
+    cin >> x;
+    x--;
+    string str = "HelloWorld";
+    str.erase(str.begin()+x);
+    cout << str;
+
+    return 0;
+}
