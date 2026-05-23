80a37d9f5c8da3b1db53fc12ba0501c01c35d336
Saksham280504 <sakshammodi47@gmail.com>
2026-05-23 17:44:29 +0530

    Problem Solved

--- contests/atCoder/atCoder02/tempCodeRunnerFile.cpp
+++ contests/atCoder/atCoder02/tempCodeRunnerFile.cpp
@@ -16,12 +16,29 @@ int main() {
     // your code here
 
     // Q1
-    int x;
-    cin >> x;
-    x--;
-    string str = "HelloWorld";
-    str.erase(str.begin()+x);
-    cout << str;
+    // int x;
+    // cin >> x;
+    // x--;
+    // string str = "HelloWorld";
+    // str.erase(str.begin()+x);
+    // cout << str;
 
+    // Q2
+    int n;
+    cin >> n;
+    while(n--) {
+        string str;
+        cin >> str;
+        int c = str[0]-'a'+1;
+        if(c<=15) {
+            if(c%3==0) cout << c/3+1;
+            else cout << c/3+2;
+        }
+        else {
+            if(c<=19) cout << 7;
+            else if(c<=22) cout << 8;
+            else cout << 9;
+        }
+    }
     return 0;
 }
