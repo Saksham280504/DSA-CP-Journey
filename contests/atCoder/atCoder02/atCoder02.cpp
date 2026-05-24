11a35bcb1e34afc6914c4970127785e98c50ab4e
Saksham280504 <sakshammodi47@gmail.com>
2026-05-24 14:34:48 +0530

    2 Contests done

--- contests/atCoder/atCoder02/atCoder02.cpp
+++ contests/atCoder/atCoder02/atCoder02.cpp
@@ -3,6 +3,46 @@ using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
 // #define endl '/n'
 
+void solve4() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    map<char,int> mpp;
+    for(char ch: s) {
+        mpp[ch]++;
+    }
+    int maxCount = 0;
+    int maxElement = -1;
+    for(auto it: mpp) {
+        if(maxCount<it.second) {
+            maxCount = it.second;
+            maxElement = it.first;
+        }
+    }
+    if(maxCount>n/2) {
+        cout << "No" << endl;
+        return;
+    }
+    else {
+        cout << "YES" << endl;
+        string str = "";
+        int cnt = 0;
+        if(n%2==0 && maxCount==(n/2)) {
+            str += maxElement;
+            cnt++;
+        }
+        while(cnt!=n) {
+            for(auto it:mpp) {
+                if(it.second!=0) {
+                    str += it.first;
+                    it.second--;
+                    cnt++;
+                }
+            }
+        }
+        cout << str << endl;
+    }
+}
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -24,21 +64,44 @@ int main() {
     // cout << str;
 
     // Q2
-    int n;
-    cin >> n;
-    while(n--) {
-        string str;
-        cin >> str;
-        int c = str[0]-'a'+1;
-        if(c<=15) {
-            if(c%3==0) cout << c/3+1;
-            else cout << c/3+2;
-        }
-        else {
-            if(c<=19) cout << 7;
-            else if(c<=22) cout << 8;
-            else cout << 9;
-        }
+    // int n;
+    // cin >> n;
+    // while(n--) {
+    //     string str;
+    //     cin >> str;
+    //     int c = str[0]-'a'+1;
+    //     if(c<=15) {
+    //         if(c%3==0) cout << c/3+1;
+    //         else cout << c/3+2;
+    //     }
+    //     else {
+    //         if(c<=19) cout << 7;
+    //         else if(c<=22) cout << 8;
+    //         else cout << 9;
+    //     }
+    // }
+
+    // Q3
+    // int n,q;
+    // cin >> n >> q;
+    // vector<int> blocks[q];
+    // int count = 0;
+    // while(q--) {
+    //     int type, action;
+    //     cin >> type >> action;
+    //     if(type==1) {
+            
+    //     }
+    //     else {
+
+    //     }
+    // }
+
+    // Q4
+    int t;
+    cin >> t;
+    while(t--) {
+        solve4();
     }
     return 0;
 }
---
+++ contests/leetcode/Leetcode01/Leetcode01.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+// Q1
+class Solution {
+public:
+    vector<int> limitOccurrences(vector<int>& nums, int k) {
+        map<int,int> mpp;
+        for(auto x: nums) mpp[x]++;
+        vector<int> ans;
+        for(auto it: mpp) {
+            int freq = min(it.second,k);
+            while(freq--) ans.push_back(it.first);
+        }
+        return ans;
+    }
+};
+
+// Q2
+class Solution {
+public:
+    int passwordStrength(string str) {
+        unordered_map<char,int> mpp;
+        int points = 0;
+        for(char x: str) {
+            mpp[x]++;
+        }
+        for(auto it: mpp) {
+            if('a'<= it.first && it.first<='z') points += 1;
+            else if('A'<=it.first && it.first<='Z') points += 2;
+            else if('0'<=it.first && it.first<='9') points += 3;
+            else if(it.first=='!' || it.first=='@' || it.first =='#' || it.first=='$') points += 5;
+        }
+
+        return points;
+    }
+};
+
+// Q3 (My Solution)
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return 0;
+        int position = -1;
+        for(int i=1; i<n-1; i++) {
+            if(nums[i]==0) position = i;
+            int diff1 = abs(nums[i]-nums[i-1]);
+            int diff2 = abs(nums[i]-nums[i+1]);
+            if(diff1==1 && diff2==1) continue;
+            else if(diff1==n-1 && diff2==1) continue;
+            else if(diff2==n-1 && diff1==1) continue;
+            else return -1;
+        }
+        if(position==-1) {
+            if(nums[0]==0) position = 0;
+            else if(nums[n-1]==0) position = n-1;
+        }
+        if(position<n-1 && nums[position+1]==1) return min(position,2+n-position);
+        if(position<n-1 && nums[position+1]==n-1) return min(position+2,n-position);
+        if(position==n-1 && nums[position-1]==1) return 1;
+        return min(n-1,3);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+
+    return 0;
+}
