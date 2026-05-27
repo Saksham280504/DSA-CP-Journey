7b1a7a1a903e1f28cbeb6cb0f99cff0c8663fb15
Saksham280504 <sakshammodi47@gmail.com>
2026-05-27 23:34:36 +0530

    LeetCode Contest Complete

--- contests/leetcode/Leetcode01/Leetcode01.cpp
+++ contests/leetcode/Leetcode01/Leetcode01.cpp
@@ -66,6 +66,87 @@ public:
 };
 
 // Q4 
+class Solution {
+public:
+    vector<int> numberOfPairs(vector<int>& nums1, vector<int>& nums2, vector<vector<int>>& queries) {
+        int n = nums2.size();
+        int blockSize = max(1,int(sqrt(n)));
+        int numBlocks = (n+blockSize-1)/blockSize;
+
+        // SquareRoot Decomposition Structures
+        vector<unordered_map<long long,int>> blockMap(numBlocks);
+        vector<long long> lazy(numBlocks,0);
+
+        for(int i=0; i<n; i++) {
+            int bId = i/blockSize;
+            blockMap[bId][nums2[i]]++;
+        }
+
+        vector<int> result;
+
+        for(auto q: queries) {
+            int type = q[0];
+            if(type==1) {
+                int x = q[1];
+                int y = q[2];
+                long long val = q[3]; // This line prevents run time error
+
+                int startBlock = x/blockSize;
+                int endBlock = y/blockSize;
+
+                if(startBlock==endBlock) {
+                    // Update partially covered single block
+                    for(int i=x; i<=y; i++) {
+                        blockMap[startBlock][nums2[i]]--;
+                        nums2[i] += val;
+                        blockMap[startBlock][nums2[i]]++;
+                    }
+                }
+                else {
+                    // Updating partial start block
+                    int endOfStart = (startBlock+1)*blockSize-1;
+                    for(int i=x; i<=endOfStart; i++) {
+                        blockMap[startBlock][nums2[i]]--;
+                        nums2[i] += val;
+                        blockMap[startBlock][nums2[i]]++;
+                    }
+                    
+                    // Updating complete intermediate blocks
+                    for(int i=startBlock+1; i<endBlock; i++) {
+                        lazy[i] += val;
+                    }
+            
+                    // Updating partial end Block
+                    int startOfEnd = (endBlock)*blockSize;
+                    for(int i=startOfEnd; i<=y; i++) {
+                        blockMap[endBlock][nums2[i]]--;
+                        nums2[i] += val;
+                        blockMap[endBlock][nums2[i]]++;
+                    }
+                }
+            }
+            else {
+                long long tot = q[1];
+                int totalPairs = 0;
+                for(int m: nums1) {
+                    long long targetInNums2 = tot - m;
+                    for(int b=0; b<numBlocks; b++) {
+                        int neededRawValue = targetInNums2-lazy[b];
+                        auto it = blockMap[b].find(neededRawValue);
+                        if(it!=blockMap[b].end()) {
+                            totalPairs += blockMap[b][neededRawValue];
+                        }
+                    }
+                }
+                result.push_back(totalPairs);
+            }
+        }
+        return result;
+    }
+};
+
+
+// Gemini Chat Link :- https://gemini.google.com/app/b8a6a9626c7ddc2a?is_sa=1&is_sa=1&android-min-version=301356232&ios-min-version=322.0&campaign_id=bkws&pt=9008&mt=8&ct=p-growth-sem-bkws
 
 int main() {
     ios::sync_with_stdio(0);
