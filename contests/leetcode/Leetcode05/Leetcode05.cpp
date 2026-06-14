7f7dba6b751c7d7a021604b675691b6a16d89f58
Saksham280504 <sakshammodi47@gmail.com>
2026-06-14 09:23:37 +0530

    Problem02 Solved

--- contests/leetcode/Leetcode05/Leetcode05.cpp
+++ contests/leetcode/Leetcode05/Leetcode05.cpp
@@ -16,6 +16,37 @@ public:
     }
 };
 
+class Solution {
+public:
+    int getLength(vector<int>& arr) {
+        int n = arr.size();
+        int maxBalancedArrSize = 1;
+        for(int i=0; i<n; i++) {
+            int maxFreq = 0;
+            unordered_map<int,int> mpp;
+            for(int j=i; j<n; j++) {
+                mpp[arr[j]]++;
+                maxFreq = max(maxFreq,mpp[arr[j]]);
+                bool isBalanced = (maxFreq%2==0);
+                int cnt = 0;
+                if(isBalanced) {
+                    for(auto it: mpp) {
+                        if(it.second==maxFreq) {
+                            cnt++;
+                            continue;
+                        }
+                        if(it.second==(maxFreq/2)) continue;
+                        isBalanced = false;
+                    }
+                    if(cnt>1 && cnt==mpp.size()) isBalanced = false;
+                }
+                if(isBalanced) maxBalancedArrSize = max(maxBalancedArrSize,j-i+1);
+            }
+        }
+        return maxBalancedArrSize;
+    }
+};
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
