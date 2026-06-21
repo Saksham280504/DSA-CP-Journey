0f3483b589f32e85057bb717f1a1c781968c0505
Saksham280504 <sakshammodi47@gmail.com>
2026-06-21 08:46:15 +0530

    Problem Solved

---
+++ Question-Practice/CSES/Sliding Window/P05/P05.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+#define ll long long
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+    int n,k;
+    cin >> n >> k;
+    vector<ll> arr(n);
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    map<ll,ll> mpp;
+    int i=0;
+    for(int j=0; j<n; j++) {
+        mpp[arr[j]]++;
+        if(j-i+1<k) continue;
+        else {
+            cout << mpp.size() << " ";
+            mpp[arr[i]]--;
+            if(mpp[arr[i]]==0) mpp.erase(arr[i]);
+            i++;
+        }
+    }
+    cout << endl;
+    // TC -> O(NlogK)
+    return 0;
+}
