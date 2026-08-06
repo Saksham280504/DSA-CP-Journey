6c474903d728a30c7c394a3004fa1025f6116de7
Saksham280504 <sakshammodi47@gmail.com>
2026-08-07 00:09:25 +0530

    Problem Solved

--- Question-Practice/CSES/Dynamic Programming/P10_Edit_Distance/P10_Edit_Distance.cpp
+++ Question-Practice/CSES/Dynamic Programming/P10_Edit_Distance/P10_Edit_Distance.cpp
@@ -14,8 +14,25 @@ int main() {
 #endif
 
     // your code here
-    int n;
-    cin >> n;
+    string a,b;
+    cin >> a >> b;
+    int n = a.size();
+    int m = b.size();
+    vector<int> prev(m+1,0);
+    for(int j=1; j<=m; j++) {
+        prev[j] = j;
+    }
+    for(int i=1; i<=n; i++) {
+        vector<int> curr(m+1,0);
+        curr[0] = i;
+        for(int j=1; j<=m; j++) {
+            int ops = prev[j-1] + (a[i-1]!=b[j-1]); // replaced or equal
+            ops = min(ops,min(curr[j-1],prev[j]) + 1); // either delete ith character from a (same as adding 1 character to b) or jth character from b (same as adding 1 character to a).
+            curr[j] = ops;
+        }
+        prev = curr;
+    }
+    cout << prev[m] << endl;
 
     return 0;
 }
