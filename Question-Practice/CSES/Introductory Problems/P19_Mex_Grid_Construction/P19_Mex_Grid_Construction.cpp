33595775e3399b884a82a1794eaead8a3e1365c6
Saksham280504 <sakshammodi47@gmail.com>
2026-08-16 17:14:05 +0530

    Problem Solved

---
+++ Question-Practice/CSES/Introductory Problems/P19_Mex_Grid_Construction/P19_Mex_Grid_Construction.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long  => when use this convert int main()  to int32_t main()
+// #define endl '/n'
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // your code here
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) cout << (i^j) << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
