b027f4a1ae6ae470ec858fb30e610177f56e00dd
Saksham280504 <sakshammodi47@gmail.com>
2026-08-17 18:36:12 +0530

    Problem Solved

--- Question-Practice/CSES/Introductory Problems/P24_Grid_Path_Description/P24_Grid_Path_Description.cpp
+++ Question-Practice/CSES/Introductory Problems/P24_Grid_Path_Description/P24_Grid_Path_Description.cpp
@@ -10,15 +10,16 @@ int ans = 0;
 int dr[4] = {-1,0,1,0};
 int dc[4] = {0,1,0,-1};
 
+
 void dfs(int r, int c, int idx) {
     if(r==7 && c==1) {
         if(idx==48) ans++;
         return;
     }
+
     if(idx==48) return;
 
-
-    // Pruning: path splits grid into two parts
+    // Pruning: Path splits the grid in 2 parts
     if(vis[r-1][c] && vis[r+1][c] && !vis[r][c-1] && !vis[r][c+1]) return;
     if(vis[r][c-1] && vis[r][c+1] && !vis[r-1][c] && !vis[r+1][c]) return;
 
@@ -30,9 +31,7 @@ void dfs(int r, int c, int idx) {
         for(int i=0; i<4; i++) {
             int nr = r + dr[i];
             int nc = c + dc[i];
-            if(!vis[nr][nc]) {
-                dfs(nr,nc,idx+1);
-            }
+            if(!vis[nr][nc]) dfs(nr,nc,idx+1);
         }
     }
     else {
@@ -45,8 +44,10 @@ void dfs(int r, int c, int idx) {
         int nc = c + dc[k];
         if(!vis[nr][nc]) dfs(nr,nc,idx+1);
     }
+
     vis[r][c] = false;
 }
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -57,15 +58,15 @@ int main() {
     freopen("output.txt", "w", stdout);
 #endif
 
-    // Write your code here
+    // your code here
     cin >> s;
     for(int i=0; i<9; i++) {
-        vis[i][0] = vis[i][8] = true;
         vis[0][i] = vis[8][i] = true;
+        vis[i][0] = vis[i][8] = true;
     }
     dfs(1,1,0);
 
-    cout << ans << endl;
+    cout << ans  << endl;
 
     return 0;
 }
