3d3c8b4d577a4f4af511cd4a77e1e047fd4a606b
Saksham280504 <sakshammodi47@gmail.com>
2026-08-29 16:21:33 +0530

    Problem Solved

--- Question-Practice/Sultaniya_Sheet/LeetCode/P1632_Rank_Transform_Of_A_Matrix/P1632_Rank_Transform_Of_A_Matrix.cpp
+++ Question-Practice/Sultaniya_Sheet/LeetCode/P1632_Rank_Transform_Of_A_Matrix/P1632_Rank_Transform_Of_A_Matrix.cpp
@@ -4,9 +4,46 @@ using namespace std;
 // #define endl '/n'
 
 class Solution {
+private:
+    int find(vector<int>& parent, int i) { // Compression
+        return (parent[i]==i) ? parent[i] : parent[i] = find(parent,parent[i]);
+    }
 public:
     vector<vector<int>> matrixRankTransform(vector<vector<int>>& matrix) {
-        
+        int n = matrix.size(), m = matrix[0].size();
+        map<int,vector<pair<int,int>>> mpp;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                mpp[matrix[i][j]].push_back({i,j});
+            }
+        }
+        // This groups all the cells in ascending order of the values they are holding
+
+        vector<int> rank(m+n,0);
+        vector<vector<int>> ans(m,vector<int>(n));
+        for(auto [val,cells]: mpp) {
+            vector<int> parent(m+n);
+            iota(parent.begin(), parent.end(), 0);
+            auto root = [&](int i) { // Lambda function
+                return find(parent,i);
+            };
+            for(auto &cell: cells) { // Grouping of all cells under one connected component
+                int i = cell.first, j = cell.second;
+                parent[root(i)] = root(j+n);
+            }
+            vector<int> current_rank = rank;
+            for(auto &cell: cells) {
+                int i = cell.first, j = cell.second;
+                int r = root(i);
+                current_rank[r] = max({current_rank[r],rank[i],rank[j+n]});
+            }
+            for(auto &cell: cells) {
+                int i = cell.first, j = cell.second;
+                int r = root(i);
+                ans[i][j] = rank[i] = rank[j+n] = current_rank[r]+1;
+            }
+        }
+        return ans;
     }
 };
 
--- Question-Practice/Sultaniya_Sheet/LeetCode/P1696_Jump_Game_6/P1696_Jump_Game_6.cpp
+++ Question-Practice/Sultaniya_Sheet/LeetCode/P1696_Jump_Game_6/P1696_Jump_Game_6.cpp
@@ -1,8 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
-// #define endl '
-'
+// #define endl '/n'
+
+// class Solution {
+// public:
+//     int maxResult(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         vector<int> dp(n);
+//         dp[0] = nums[0]; // Base-Case
+//         deque<int> dq;
+//         dq.push_back(0);
+
+//         for(int i=1; i<n; i++) {
+//             while(!dq.empty() && dq.front()<i-k) dq.pop_front();
+
+//             dp[i] = nums[i] + dp[dq.front()];
+
+//             while(!dq.empty() && dp[dq.back()]<=dp[i]) dq.pop_back();
+
+//             dq.push_back(i);
+//         }
+
+//         return dp[n-1];
+//     }
+// };
+
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> dp(n,-1e9);
+        deque<int> dq; // dq will contain the largest maximum scores in the k-size window in descending order
+        dq.push_back(0);
+        dp[0] = nums[0];
+        for(int i=1; i<n; i++) {
+            while(!dq.empty() && dq.front()<i-k) dq.pop_front();
+            dp[i] = nums[i] + dp[dq.front()];
+            while(!dq.empty() && dp[dq.back()]<=dp[i]) dq.pop_back();
+            dq.push_back(i);
+        }
+        return dp[n-1];
+    }
+};
 
 int main() {
     ios::sync_with_stdio(0);
