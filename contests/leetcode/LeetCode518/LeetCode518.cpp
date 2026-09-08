50010d52ddeb8e19f00a733cb5905d282b35a6a3
Saksham280504 <sakshammodi47@gmail.com>
2026-09-08 13:51:03 +0530

    Problem Solved

--- contests/leetcode/LeetCode518/LeetCode518.cpp
+++ contests/leetcode/LeetCode518/LeetCode518.cpp
@@ -1,8 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
-// #define endl '
-'
+// #define endl '/n'
+
+// Q1
+class Solution {
+public:
+    int countRotations(string s, int k) {
+        int ans = 0;
+        int n = s.size();
+        do {
+            int cnt = 0;
+            for(int i=0; i<(s.size()-1); i++) {
+                if(s[i]==s[i+1]) cnt++;
+            }
+            if(cnt==k) ans++;
+            auto it = s.begin();
+            char ch = *it;
+            s.erase(it);
+            s += ch;
+            n--;
+        }
+        while(n>0);
+        return ans;
+    }
+};
 
 int main() {
     ios::sync_with_stdio(0);
