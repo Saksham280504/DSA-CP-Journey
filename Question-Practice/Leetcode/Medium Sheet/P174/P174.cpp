4dd6f191826df9a1ef6386cfbdd6638c2346b891
Saksham280504 <sakshammodi47@gmail.com>
2026-09-10 16:56:37 +0530

    Problem Solved

--- Question-Practice/Leetcode/Medium Sheet/P174/P174.cpp
+++ Question-Practice/Leetcode/Medium Sheet/P174/P174.cpp
@@ -1,8 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
-// #define endl '
-'
+// #define endl '/n'
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int first = INT_MAX;
+        int second = INT_MAX;
+        for(int x: nums) {
+            if(x<=first) first = x;
+            else if(x<=second) second = x;
+            else return true;
+        }
+        return false;
+    }
+};
 
 int main() {
     ios::sync_with_stdio(0);
