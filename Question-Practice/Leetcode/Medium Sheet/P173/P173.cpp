352ccf72d2d192cb00e6fa6726d45a6d12ba01a2
Saksham280504 <sakshammodi47@gmail.com>
2026-09-10 16:56:07 +0530

    Problem Solved

--- Question-Practice/Leetcode/Medium Sheet/P173/P173.cpp
+++ Question-Practice/Leetcode/Medium Sheet/P173/P173.cpp
@@ -1,8 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
-// #define endl '
-'
+// #define endl '/n'
+
+class Solution {
+public:
+    int maxLength(vector<string>& arr) {
+        int n = arr.size();
+        int total = (1<<n);
+        int maxSize = 0;
+        for(int mask=0; mask<total; mask++) {
+            set<char> st;
+            for(int i=0; i<n; i++) {
+                if(mask&(1<<i)) {
+                    string &str = arr[i];
+                    for(char &ch: str) {
+                        if(st.count(ch)) {
+                            st.clear();
+                            break;
+                        }
+                        st.insert(ch);
+                    }
+                    if(st.size()==0) break;
+                    int setSize = st.size();
+                    maxSize = max(maxSize,setSize);
+                }
+            }
+        }
+        return maxSize;
+    }
+};
 
 int main() {
     ios::sync_with_stdio(0);
