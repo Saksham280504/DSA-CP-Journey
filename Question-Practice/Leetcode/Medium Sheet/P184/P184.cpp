040c552e251a857cdfbaaa37db9ce593cfa24d9b
Saksham280504 <sakshammodi47@gmail.com>
2026-09-12 23:27:31 +0530

    Problem Solved

--- Question-Practice/Leetcode/Medium Sheet/P184/P184.cpp
+++ Question-Practice/Leetcode/Medium Sheet/P184/P184.cpp
@@ -3,7 +3,19 @@ using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
 // #define endl '/n'
 
-
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0) return 0;
+        int up=1,down=1;
+        for(int i=1; i<n; i++) {
+            if(nums[i]>nums[i-1]) up = down+1;
+            else if(nums[i]<nums[i-1]) down = up+1;
+        }
+        return max(up,down);
+    }
+};
 
 int main() {
     ios::sync_with_stdio(0);
