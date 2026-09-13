1cd9f70f6d5088eb052cc5188123389d2736943b
Saksham280504 <sakshammodi47@gmail.com>
2026-09-13 20:17:26 +0530

    Problem Solved

--- Question-Practice/Leetcode/Medium Sheet/P190/P190.cpp
+++ Question-Practice/Leetcode/Medium Sheet/P190/P190.cpp
@@ -3,6 +3,32 @@ using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
 // #define endl '/n'
 
+class Solution {
+private:
+    bool isPrime(int val) {
+        if(val<2) return false;
+        if(val==2 || val==3) return true;
+        if(val%2==0 || val%3==0) return false;
+        for(int i=5; i*i<=val; i+=6) {
+            if(val%i==0 || val%(i+2)==0) return false;
+        }
+        return true;
+    }
+public:
+    int primePalindrome(int n) {
+        if(n>=8 && n<=11) return 11;
+        for(int i=0; i<=1e5; i++) {
+            string s = to_string(i);
+            string r = s;
+            r.pop_back();
+            reverse(r.begin(),r.end());
+            int pal = stoi(s+r);
+            if(pal >=n && isPrime(pal)) return pal;
+        }
+        return -1;
+    }
+};
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
