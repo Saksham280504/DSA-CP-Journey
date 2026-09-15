a91eb60950bfafe86425fecc6ee182caa1d9ddb8
Saksham280504 <sakshammodi47@gmail.com>
2026-09-16 02:31:23 +0530

    Problem Solved

--- Question-Practice/Leetcode/Medium Sheet/P207/P207.cpp
+++ Question-Practice/Leetcode/Medium Sheet/P207/P207.cpp
@@ -1,8 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 // #define int long long  => when use this convert int main()  to int32_t main()
-// #define endl '
-'
+// #define endl '/n'
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        int l=0, r=0, maxLen=0, maxFreq=0;
+        vector<int> hash(26,0);
+        while(r<n) {
+            hash[s[r]-'A']++;
+            maxFreq = max(maxFreq,hash[s[r]-'A']);
+            if(r-l+1-maxFreq>k) {
+                hash[s[l]-'A']--;
+                l++;
+                maxFreq = max(maxFreq,hash[s[l]-'A']);
+            }
+            if(r-l+1-maxFreq<=k) {
+                maxLen = max(maxLen,r-l+1);
+            }
+            r++;
+        }
+        return maxLen;
+    }
+};
 
 int main() {
     ios::sync_with_stdio(0);
